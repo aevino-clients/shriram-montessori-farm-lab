2e4e20cc50cd69d9df7ae8c3e85c9a63df01257b
aevino <aevino2025@gmail.com>
2025-06-20 12:43:18 +0530

    update

--- src/components/ClosingSection/index.tsx
+++ src/components/ClosingSection/index.tsx
@@ -7,7 +7,7 @@ const ClosingSection = () => {
 				<div className="-mx-4 flex flex-wrap">
 					<div
 						data-aos="fade-up-subtle"
-						className="w-full md:w-1/2 lg:w-1/2 px-4 h-[300px] flex relative"
+						className="w-full md:w-1/2 lg:w-1/2 px-4 h-[200px] flex relative md:h-[300px] lg:h-[300px] xl:h-[300px] 2xl:h-[300px]"
 					>
 						<Image
 							alt="farm"
